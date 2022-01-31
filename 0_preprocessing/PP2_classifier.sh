4afd424fe7df79978290815a9b2053326e386c24
Mass23 <33321080+Mass23@users.noreply.github.com>
2022-01-31 07:53:16 +0100

    Update PP2_classifier.sh

--- 0_preprocessing/PP2_classifier.sh
+++ 0_preprocessing/PP2_classifier.sh
@@ -4,6 +4,6 @@ wget https://data.qiime2.org/2020.8/common/silva-138-99-tax.qza > silva-138-99-t
 
 conda activate qiime2-2020.8
 
-qiime feature-classifier extract-reads --i-sequences silva-138-99-seqs.qza --p-f-primer GTGCCAGCMGCCGCGGTAA --p-r-primer GGACTACHVGGGTWTCTAAT --p-min-length XXX --p-max-length XXX --o-reads silva-138-99_515f-806r_400-450_reads.qza
+qiime feature-classifier extract-reads --i-sequences silva-138-99-seqs.qza --p-f-primer GTGCCAGCMGCCGCGGTAA --p-r-primer GGACTACHVGGGTWTCTAAT --p-min-length 200 --p-max-length 400 --o-reads silva-138-99_515f-806r_400-450_reads.qza
 
 qiime feature-classifier fit-classifier-naive-bayes --i-reference-reads silva-138-99_515f-806r_400-450_reads.qza --i-reference-taxonomy silva-138-99-tax.qza --o-classifier silva-138-99_515f-806r_400-450_classifier.qza
